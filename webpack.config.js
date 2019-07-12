2ad861e8d372a037daeb9662051e0309629aff4a
DirtySoc <derek.willingham@outlook.com>
2019-07-12 18:04:58 -0400

    completed lecture 86

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/app.jsx',
+    entry: './src/playground/redux-101.jsx',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
